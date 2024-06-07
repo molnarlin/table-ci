a1d8675131593a9dfe0fb57992586e73544400a6
Katalin Molnar <molnarlin@gmail.com>
2024-06-07 10:57:13 +0100

    class selector added

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    $('h2').css('text-decoration', 'underline');
-    $('ul').css('border', 'solid 1px #ccc');
+    $('h2').addClass('underline');
+    $('ul').addClass('border');
 });
